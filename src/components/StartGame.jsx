ada6d9a2da8b42888b9f25e2a485c250f38028e8
Balaji Laygude <balajiwork05@gmail.com>
2026-08-19 09:39:51 +0530

    layout set

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -4,10 +4,10 @@ export default function StartGame({toggle}) {
 
 
   return (
-    <div className="min-w-screen min-h-screen md:flex justify-around items-center">
+    <div className="min-w-screen my-auto min-h-screen md:flex justify-around items-center px-5 py-10">
     <img src="./images/dices 1.png" alt="Dices" 
       className="" />
-      <div className=' flex flex-col justify-center items-center md:items-start'>
+      <div className=' flex py-10 flex-col justify-center items-center md:items-start'>
         <h1 className='text-7xl text-center font-extrabold mb-5'>DICE GAME</h1>
         <button onClick={toggle}
          className='w-36 h-12 bg-black hover:bg-gray-800  text-white rounded-sm'>Play</button>
